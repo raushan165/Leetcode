07b35ead90aaf16d8cd36afa938e9955526ec5eb
Raushan Kumar <152487774+raushan165@users.noreply.github.com>
2026-07-14 15:59:16 +0530

    Time: 113 ms (18.59%), Space: 182.2 MB (28.3%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,76 +1,45 @@
+#include <unordered_map>
+#include <list>
+using namespace std;
+
 class LRUCache {
 public:
-    class Node{
-        public: 
-            int key;
-            int val;
-            Node* prev;
-            Node* next;
-
-            Node(int key, int val){
-                this->key = key;
-                this->val = val;
-            }
-    };
-
-    Node* head = new Node(-1, -1);
-    Node* tail = new Node(-1, -1);
-
     int cap;
-    unordered_map<int, Node*> m;
-
+    list<int> dll;   
+    unordered_map<int, pair<int, list<int>::iterator>> mp;
     LRUCache(int capacity) {
         cap = capacity;
-        head -> next = tail;
-        tail -> prev = head;
     }
-
-    void addNode(Node* newnode){
-        Node* temp = head -> next;
-
-        newnode -> next = temp;
-        newnode -> prev = head;
-
-        head -> next = newnode;
-        temp -> prev = newnode;
-    }
-
-    void deleteNode(Node* delnode){
-        Node* prevv = delnode -> prev;
-        Node* nextt = delnode -> next;
-
-        prevv -> next = nextt;
-        nextt -> prev = prevv;
-    }
-    
     int get(int key) {
-        if(m.find(key) != m.end()){
-            Node* resNode = m[key];
-            int ans = resNode -> val;
+        if (mp.find(key) == mp.end())
+            return -1;
+        dll.erase(mp[key].second);
+        dll.push_front(key);
+        mp[key].second = dll.begin();
 
-            m.erase(key);
-            deleteNode(resNode);
-            addNode(resNode);
-
-            m[key] = head -> next;
-            return ans;
-        }
-        return -1;
+        return mp[key].first;
     }
-    
     void put(int key, int value) {
-        if(m.find(key) != m.end()){
-            Node* curr = m[key];
-            m.erase(key);
-            deleteNode(curr);
+        if (mp.find(key) != mp.end()) {
+            dll.erase(mp[key].second);
+            dll.push_front(key);
+            mp[key] = {value, dll.begin()};
         }
-
-        if(m.size() == cap){
-            m.erase(tail -> prev -> key);
-            deleteNode(tail -> prev);
+        else {
+            if (dll.size() == cap) {
+                int lru = dll.back();
+                dll.pop_back();
+                mp.erase(lru);
+            }
+            dll.push_front(key);
+            mp[key] = {value, dll.begin()};
         }
-
-        addNode(new Node(key, value));
-        m[key] = head -> next;
     }
 };
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
